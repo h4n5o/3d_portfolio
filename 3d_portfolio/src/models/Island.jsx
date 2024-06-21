d85e46d6e32c4bce9a6dc51b66438eddbdc0499a
h4n5o <72876852+h4n5o@users.noreply.github.com>
2024-06-21 11:09:21 +0200

    Minor Changes in Code and More functionality for later added.

--- 3d_portfolio/src/models/Island.jsx
+++ 3d_portfolio/src/models/Island.jsx
@@ -15,10 +15,68 @@ import { a } from '@react-spring/three'
 import islandScene from '../assets/3d/island.glb';
 
 
-const Island = ( props ) => {
+const Island = ({ isRotating, setIsRotating, ...props } ) => {
     const islandRef = useRef();
+
+    const { gl, viewport } = useThree();
     const { nodes, materials } = useGLTF(islandScene);
-  return (
+  
+    const lastX = useRef(0);
+    const rotationSpeed = useRef(0);
+    const dampingFactor = 0.95;
+
+    const handlePointerDown = (e) => {
+      e.stopPropragation();
+      e.preventDefault();
+      setIsRotating(true);
+
+      const clientX = event.touches 
+      ? e.touches[0].clientX 
+      : e.clientX;
+      lastX.current = clientX;
+    }
+    const handlePointerUp = (e) => {
+      e.stopPropragation();
+      e.preventDefault();
+      setIsRotating(false);
+
+      const clientX = e.touches 
+      ? e.touches[0].clientX 
+      : e.clientX;
+      
+      const delta = (clientX - lastX.current) / viewport.width;
+
+      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+      lastX.current = clientX;
+
+      rotationSpeed.current = delta * 0.01 * Math.PI;
+    }
+    const handlePointerMove = (e) => {
+      e.stopPropragation();
+      e.preventDefault();
+
+      if(isRotating) {
+        handlePointerUp(e);
+
+      }
+      
+    }
+
+    useEffect (() => {
+      document.addEventListener('pointerdown', 'handlePointerDown');
+      document.addEventListener('pointerup', 'handlePointerUp');
+      document.addEventListener('pointermove', 'handlePointerMove');
+
+      return () => {
+      document.addEventListener('pointerdown', 'handlePointerDown');
+      document.addEventListener('pointerup', 'handlePointerUp');
+      document.addEventListener('pointermove', 'handlePointerMove'); 
+      }
+
+    }, [gl, handlePointerDown, handlePointerUp, handlePointerMove])
+    
+    
+    return (
     <a.group ref={islandRef} {...props}>
       <mesh
         geometry={nodes.polySurface944_tree_body_0.geometry}
